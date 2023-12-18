c3892c261f5eef1a03d02259710e7b232157f535
misheck gogo <misheckgogo45@gmail.com>
2023-12-18 13:25:55 -0800

    pass

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
+    if my_list is None:
+        return 0
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
+        print()
+    except IndexError:
+        print()
+        pass
+    return count
