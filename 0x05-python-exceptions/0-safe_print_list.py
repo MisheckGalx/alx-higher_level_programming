70301097124289a29f5b16f7c3e63efaea2191bf
misheck gogo <misheckgogo45@gmail.com>
2023-12-18 13:04:52 -0800

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-def print_list_safely(my_list=[], x=0);
-  count = 0
-  for i in range(x);
-    try;
-      print(my_list[i] end="")
-      count += 1
-    except IndexError;
-      break
-  print("")
-  return
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            num += 1
+        except IndexError:
+            break
+    print("")
+    Return
