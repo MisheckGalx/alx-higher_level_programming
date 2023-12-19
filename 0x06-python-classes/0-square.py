f7a7f698a0e706564adbe121820aef13242e308c
misheck gogo <misheckgogo45@gmail.com>
2023-12-19 09:04:53 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""This is a class square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Define a square object"""
-    def __init__(self, size):
-        """Initialize method that stores the size of the square
-
-        Args:
-            param1 (int): size of the square
-        """
-        self.__size = size
+    """Represent a square."""
+    pass
