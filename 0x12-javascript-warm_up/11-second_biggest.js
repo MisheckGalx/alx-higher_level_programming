7370b67b4af2948c81c59e75f2a9cc82351cced5
misheck gogo <misheckgogo45@gmail.com>
2024-03-11 14:14:27 -0700

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 
-// A script that searches the second biggest integer in the list of arguments
+const process = require('process');
+const args = process.argv;
 
-const entries = process.argv.slice(2).map(Number);
-let max = 0;
-
-// entries.forEach((val, index) => {
-// console.log(`${index}: ${val}`);
-// });
-
-if (entries.length === 0) {
-  max = 0;
-  console.log(max);
-} else if (entries.length === 1) {
-  max = 0;
-  console.log(max);
+if (isNaN(args[2]) || isNaN(args[3])) {
+  console.log('0');
 } else {
-  entries.sort((a, b) => b - a);
-  console.log(entries[1]);
+  const array = args.map(Number);
+  array.slice(2, args.length);
+  array.sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
 }
