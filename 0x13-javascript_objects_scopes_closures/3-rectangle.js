3982e2a22d901a4d095e21a7d3f4adf4b7c73959
misheck gogo <misheckgogo45@gmail.com>
2024-03-12 06:14:51 -0700

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
-/*
- * a class Rectangle that defines a rectangle
- */
-
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      //an empty object if w or h is not a positive integer//
       return {};
     }
 
@@ -15,7 +10,6 @@ class Rectangle {
   }
 
   print() {
-    //the rectangle using the character X//
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
