a3d6405168686882ccd8b14c84a3e6766d057eb5
misheck gogo <misheckgogo45@gmail.com>
2024-03-12 03:42:31 -0700

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+ * a class Rectangle that defines a rectangle
+ */
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    }
+
+    this.width = w;this.height = h;
+  }
+}
+
+module.exports = Rectangle;
