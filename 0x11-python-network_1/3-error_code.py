0b86534c6cd769a0ef961c19e44ba9fa776b0468
misheck gogo <misheckgogo45@gmail.com>
2024-03-28 03:58:53 -0700

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Using urllib to send a request"""
+
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    # Check the URL from the command line argument
+    url = sys.argv[1]
+
+    try:
+        # Make a GET request to the URL
+        with request.urlopen(url) as response:
+            response_body = response.read().decode('utf-8')print(response_body)
+
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
