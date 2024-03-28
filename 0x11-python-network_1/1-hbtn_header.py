03953b2e867ec72bbb26206b11e701e6c249ef1a
misheck gogo <misheckgogo45@gmail.com>
2024-03-28 03:31:48 -0700

    1. Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""1-hbtn_header module"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    """
+     This script retrieves the value of the X-Request-Id
+    header from the response of a given URL.
+	"""
+
+    # Obtain the URL from the command-line argument
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    # Request to the URL and retrieve the response
+    with urllib.request.urlopen(req) as response:
+        x_request_id = response.getheader("X-Request-Id")
+
+        print("{}".format(x_request_id))
