46c842c5726d3d8452383e4de181cd53de2b23bc
misheck gogo <misheckgogo45@gmail.com>
2024-04-24 03:06:41 -0700

    Write a script that prints the number of movies where the character “Wedge Antilles” is present.Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,23 @@
 #!/usr/bin/node
-// 4-starwars_count.js
 
-// A script that prints the number of movies where the character “Wedge Antilles” is present.
+// import the module
 const request = require('request');
-const processArgv = require('process').argv;
-const url = processArgv[2];
 
-request.get(url, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+// The first argument is the API URL
+const apiUrl = process.argv[2];
 
-  if (response.statusCode === 200) {
-    const responseData = JSON.parse(body);
-    const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-    let count = 0;
+// Make an HTTP GET request to the API
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    // parse the JSON response
+    const results = JSON.parse(body).results;
 
-    if (responseData.results) {
-      for (let i = 0; i < responseData.results.length; i++) {
-        const characters = responseData.results[i].characters;
-        if (characters.includes(characterUrl)) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+    // count no of movies where "Wedge Antilles" is present
+    const moviesWithWedge = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0); // initiliazing count to zero
+    console.log(moviesWithWedge);
   }
 });
